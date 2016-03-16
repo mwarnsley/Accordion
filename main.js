ccb2af266c2037c4ad4ce4717c1f685f07871867
mwarnsley <marcus.warnsley@gmail.com>
2016-03-16 10:14:03 -0400

    setting up the javascript for the accordion

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-ECHO is on.
+$(document).ready(function(){
+	(function(){
+	  var content = $("p").hide();
+	  $("h1").on("click", function(){
+	    var $that = $(this);
+	    $that.each(function(){
+	    var that2 = $that.next();
+	    $("p").not(that2).hide();
+	    that2.slideToggle();
+	    });
+	  });
+	})();
+});
